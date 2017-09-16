6660636c8c1d24f517cdb6a0b9d063e60e75d793
pywako <wongaliceyy@gmail.com>
2017-09-16 14:54:00 +0200

    nettoyage datepicker

--- web/js/datepickerBirth.js
+++ web/js/datepickerBirth.js
@@ -1,38 +1,26 @@
+$(function () {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth();
+    var day = now.getDate();
+    var hours = now.getHours();
+    var minutes = now.getSeconds();
 
-/*
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Aujourd\'hui',
-    clear: 'annuler',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-});*/
+    $('.datepicker').pickadate({
 
-var now = new Date();
-var year = now.getFullYear();
-var month = now.getMonth();
-var day = now.getDate();
-var hours = now.getHours();
-var minutes = now.getSeconds();
-
-$('.datepicker2').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    labelMonthNext: 'Mois suivant',
-    labelMonthPrev: 'Mois précédent',
-    labelMonthSelect: 'Selectionner le mois',
-    labelYearSelect: 'Selectionner une année',
-    monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
-    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
-    weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
-    weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
-    weekdaysLetter: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
-    today: 'Aujourd\'hui',
-    clear: 'Réinitialiser',
-    close: 'Fermer',
-    format: 'dd/mm/yyyy',
-    firstDay: 'Mon',
-    min: [year-120, month, day],
-    max: [year-1, month, day]
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 100, // Creates a dropdown of 15 years to control year
+        monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+        monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+        //weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+        weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+        weekdaysLetter: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
+        today: 'Aujourd\'hui',
+        clear: 'Réinitialiser',
+        close: 'Fermer',
+        format: 'dd/mm/yyyy',
+        firstDay: 'Mon',
+        min: [year-120, month, day],
+        max: [year-1, month, day]
+    });
 });
--- web/js/datepickerDay.js
+++ web/js/datepickerDay.js
@@ -1,52 +1,43 @@
+$(function () {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth();
+    var day = now.getDate();
+    var hours = now.getHours();
+    var minutes = now.getSeconds();
 
-/*
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Aujourd\'hui',
-    clear: 'annuler',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-});*/
-
-var now = new Date();
-var year = now.getFullYear();
-var month = now.getMonth();
-var day = now.getDate();
-var hours = now.getHours();
-var minutes = now.getSeconds();
-
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 2, // Creates a dropdown of 15 years to control year
-    labelMonthNext: 'Mois suivant',
-    labelMonthPrev: 'Mois précédent',
-    labelMonthSelect: 'Selectionner le mois',
-    labelYearSelect: 'Selectionner une année',
-    monthsFull: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
-    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
-    weekdaysFull: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
-    weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
-    weekdaysLetter: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
-    today: 'Aujourd\'hui',
-    clear: 'Réinitialiser',
-    close: 'Fermer',
-    format: 'dd/mm/yyyy',
-    firstDay: 'Mon',
-    min: new Date(),
-    max: [year+1, month, day],
-    disable: [
-        2,7,
-        [year,0,1],
-        //Lundi paque
-        [year,4,1],
-        [year,4,8],
-        // Jeudi ascension
-        // Lundi pentecôte
-        [year, 6,14],
-        [year, 7,15],
-        [year,10,1],
-        [year, 10, 11],
-        [year,11,25]
-    ]
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 2, // Creates a dropdown of 15 years to control year
+        labelMonthNext: 'Mois suivant',
+        labelMonthPrev: 'Mois précédent',
+        labelMonthSelect: 'Selectionner le mois',
+        labelYearSelect: 'Selectionner une année',
+        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthsShort: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'],
+        weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        weekdaysLetter: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+        today: 'Aujourd\'hui',
+        clear: 'Réinitialiser',
+        close: 'Fermer',
+        format: 'dd/mm/yyyy',
+        firstDay: 'Mon',
+        min: new Date(),
+        max: [year + 1, month, day],
+        disable: [
+            2, 7,
+            [year, 0, 1],
+            //Lundi paque
+            [year, 4, 1],
+            [year, 4, 8],
+            // Jeudi ascension
+            // Lundi pentecôte
+            [year, 6, 14],
+            [year, 7, 15],
+            [year, 10, 1],
+            [year, 10, 11],
+            [year, 11, 25]
+        ]
+    });
 });
