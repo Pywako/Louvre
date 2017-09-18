766ae94279e5d446b9f69db98e45d4d0b348abc5
pywako <wongaliceyy@gmail.com>
2017-09-18 13:46:40 +0200

    indentation calcul easter holiday

--- web/js/datepickerDay.js
+++ web/js/datepickerDay.js
@@ -1,10 +1,39 @@
 $(function () {
-    var now = new Date();
-    var year = now.getFullYear();
-    var month = now.getMonth();
-    var day = now.getDate();
-    var hours = now.getHours();
+    var now     = new Date();
+    var year    = now.getFullYear();
+    var month   = now.getMonth();
+    var day     = now.getDate();
+    var hours   = now.getHours();
     var minutes = now.getSeconds();
+
+    function easterForYear (year) {
+        var a = year % 19;
+        var b = Math.floor(year / 100);
+        var c = year % 100;
+        var d = Math.floor(b / 4);
+        var e = b % 4;
+        var f = Math.floor((b + 8) / 25);
+        var g = Math.floor((b - f + 1) / 3);
+        var h = (19 * a + b - d - g + 15) % 30;
+        var i = Math.floor(c / 4);
+        var k = c % 4;
+        var l = (32 + 2 * e + 2 * i - h - k) % 7;
+        var m = Math.floor((a + 11 * h + 22 * l) / 451);
+        var n0 = (h + l + 7 * m + 114)
+        var n = Math.floor(n0 / 31) - 1;
+        var p = n0 % 31 + 1;
+        var finalDate = new Date(year,n,p);
+        return finalDate;
+    }
+
+    var easter = easterForYear(year);
+    var easterMonth =easter.getMonth();
+    var easterDay =easter.getDay();
+
+    var paques = [year];
+    var ascension = [];
+    var pentecote = [];
+
 
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
@@ -37,7 +66,7 @@ $(function () {
             [year, 7, 15],
             [year, 10, 1],
             [year, 10, 11],
-            [year, 11, 25]
+            [year, 11, 25],
         ]
     });
 });
