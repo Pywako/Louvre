8706d5ae97ad0d2a56337cd8d689345b721e0eb6
pywako <wongaliceyy@gmail.com>
2017-09-24 21:10:35 +0200

    ajout stripe error

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -16,11 +16,6 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class BookingController extends Controller
 {
-    public function __construct()
-    {
-        $this->manager = new BookingManager();
-    }
-
     /**
      * @param Request $request
      * @Route("/", name="homepage")
@@ -64,7 +59,7 @@ class BookingController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             $manager->fillingTicket($booking);
-            $manager->generateTicket($request, $booking);
+            $manager->generateTicket($booking);
             return $this->redirectToRoute('step3');
         }
 
@@ -107,9 +102,31 @@ class BookingController extends Controller
                 $this->addFlash('success', 'Commande effectuée');
                 return $this->redirectToRoute('confirm');
             }
-            catch(\Exception $e){
-                $this->addFlash('error', 'Il y a une erreur');
-                dump($e);
+            catch(\Stripe\Error\Card $e) {
+                // Since it's a decline, \Stripe\Error\Card will be caught
+                $body = $e->getJsonBody();
+                $err  = $body['error'];
+
+                print('Status is:' . $e->getHttpStatus() . "\n");
+                print('Type is:' . $err['type'] . "\n");
+                print('Code is:' . $err['code'] . "\n");
+                // param is '' in this case
+                print('Param is:' . $err['param'] . "\n");
+                print('Message is:' . $err['message'] . "\n");
+            } catch (\Stripe\Error\RateLimit $e) {
+                // Too many requests made to the API too quickly
+            } catch (\Stripe\Error\InvalidRequest $e) {
+                // Invalid parameters were supplied to Stripe's API
+            } catch (\Stripe\Error\Authentication $e) {
+                // Authentication with Stripe's API failed
+                // (maybe you changed API keys recently)
+            } catch (\Stripe\Error\ApiConnection $e) {
+                // Network communication with Stripe failed
+            } catch (\Stripe\Error\Base $e) {
+                // Display a very generic error to the user, and maybe send
+                // yourself an email
+            } catch (Exception $e) {
+                // Something else happened, completely unrelated to Stripe
             }
         }
 
