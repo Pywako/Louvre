736864e743dc79e81994ad34929bd3c28a145930
pywako <wongaliceyy@gmail.com>
2017-10-06 12:29:57 +0200

    ajout test fonctionnel step 1 et 2

--- tests/AppBundle/Controller/BookingControllerTest.php
+++ tests/AppBundle/Controller/BookingControllerTest.php
@@ -10,33 +10,66 @@ namespace Tests\AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 
 class BookingControllerTest extends WebTestCase
 {
     private $client = null;
+    private $session = null;
 
     public function setUp()
     {
         $this->client = static::createClient();
         $this->client->followRedirects();
+        $this->session = new Session((new MockFileSessionStorage()));
     }
 
     public function testHomepageIsUp()
     {
         $this->client->request('GET', '/');
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
-        echo $this->client->getResponse()->getContent();
     }
 
     public function testStep1IsUp()
     {
-
+        $this->client->request('GET', '/step1');
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testStep1validation()
+    public function testStep1FormValidation()
     {
+        $crawler = $this->client->request('GET', '/step1');
 
+        $bookingForm = $crawler->selectButton('continuer')->form();
+        $bookingForm['bookingStep1[email]'] = 'wongaliceyy@gmail.com';
+        $bookingForm['bookingStep1[dateVisit]'] = 2017/12/20;
+        $bookingForm['bookingStep1[nbTicket]'] = 1;
+        $bookingForm['bookingStep1[type]'] = 1;
+        $this->session = set('booking', $bookingForm);
+        $crawler = $this->client->submit($bookingForm);
     }
 
+    public function testStep2IsUp()
+    {
+        if(isset($this->session) && !empty($this->session))
+        {
+            $this->client->request('GET', '/step2');
+            $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+        }
+    }
 
+    public function testStep2FormValidation()
+    {
+        $crawler = $this->client->request('GET', '/step2');
+
+        $ticketForm = $crawler->selectButton('continuer')->form();
+        $ticketForm['bookingStep2[nom]'] = 'test';
+        $ticketForm['bookingStep2[prenom]'] = 'unit';
+        $ticketForm['bookingStep2[dateNaissance]'] = 1990/12/01;
+        $ticketForm['bookingStep2[pays]'] = 'FR';
+        $ticketForm['reduit'] = false;
+        $this->session->get('booking')->setTickets($ticketForm);
+        $crawler = $this->client->submit($ticketForm);
+    }
 }
