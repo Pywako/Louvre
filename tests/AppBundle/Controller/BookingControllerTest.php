76b5bd3f0b0500ccbc35ae9b855c37a4cd430596
pywako <wongaliceyy@gmail.com>
2017-10-09 10:03:31 +0200

    correction tests fonctionnels

--- tests/AppBundle/Controller/BookingControllerTest.php
+++ tests/AppBundle/Controller/BookingControllerTest.php
@@ -10,66 +10,77 @@ namespace Tests\AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Client;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 
 class BookingControllerTest extends WebTestCase
 {
+    /**
+     * @var Client
+     */
     private $client = null;
     private $session = null;
 
     public function setUp()
     {
         $this->client = static::createClient();
-        $this->client->followRedirects();
         $this->session = new Session((new MockFileSessionStorage()));
     }
 
-    public function testHomepageIsUp()
+    public function UrlDataProvider()
     {
-        $this->client->request('GET', '/');
-        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+        return [
+            ['/',Response::HTTP_OK],
+            ['/step1',Response::HTTP_OK],
+            ['/step2',Response::HTTP_FOUND],
+            ['/step3',Response::HTTP_FOUND],
+            ['/confirm/019e41daf6d8eb3b729cf774ce809dca',Response::HTTP_OK]
+        ];
     }
 
-    public function testStep1IsUp()
+    /**
+     * @dataProvider UrlDataProvider
+     */
+    public function testUrl($url,$expected)
     {
-        $this->client->request('GET', '/step1');
-        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+        $this->client->request('GET', $url);
+        $this->assertSame($expected, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testStep1FormValidation()
+
+
+    public function testOrderFlow()
     {
         $crawler = $this->client->request('GET', '/step1');
 
-        $bookingForm = $crawler->selectButton('continuer')->form();
-        $bookingForm['bookingStep1[email]'] = 'wongaliceyy@gmail.com';
-        $bookingForm['bookingStep1[dateVisit]'] = 2017/12/20;
-        $bookingForm['bookingStep1[nbTicket]'] = 1;
-        $bookingForm['bookingStep1[type]'] = 1;
-        $this->session = set('booking', $bookingForm);
+        $bookingForm = $crawler->selectButton('Continuer')->form();
+        $bookingForm['booking_step1[email]'] = 'wongaliceyy@gmail.com';
+        $bookingForm['booking_step1[dateVisit]'] = "20/12/2017";
+        $bookingForm['booking_step1[nbTicket]'] = 1;
+        $bookingForm['booking_step1[type]'] = 1;
+
         $crawler = $this->client->submit($bookingForm);
-    }
 
-    public function testStep2IsUp()
-    {
-        if(isset($this->session) && !empty($this->session))
-        {
-            $this->client->request('GET', '/step2');
-            $this->assertSame(200, $this->client->getResponse()->getStatusCode());
-        }
-    }
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_FOUND);
 
-    public function testStep2FormValidation()
-    {
-        $crawler = $this->client->request('GET', '/step2');
+        $crawler = $this->client->followRedirect();
 
-        $ticketForm = $crawler->selectButton('continuer')->form();
-        $ticketForm['bookingStep2[nom]'] = 'test';
-        $ticketForm['bookingStep2[prenom]'] = 'unit';
-        $ticketForm['bookingStep2[dateNaissance]'] = 1990/12/01;
-        $ticketForm['bookingStep2[pays]'] = 'FR';
-        $ticketForm['reduit'] = false;
-        $this->session->get('booking')->setTickets($ticketForm);
+        $ticketForm = $crawler->selectButton('Continuer')->form();
+        $ticketForm['booking_step2[tickets][0][nom]'] = 'test';
+        $ticketForm['booking_step2[tickets][0][prenom]'] = 'unit';
+        $ticketForm['booking_step2[tickets][0][dateNaissance]'] = '01/12/1990';
+        $ticketForm['booking_step2[tickets][0][pays]'] = 'FR';
+        $ticketForm['booking_step2[tickets][0][reduit]'] = false;
+
         $crawler = $this->client->submit($ticketForm);
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_FOUND);
+
+
     }
+
+
+
+
 }
