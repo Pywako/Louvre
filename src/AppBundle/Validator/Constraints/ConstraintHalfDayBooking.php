cbe915ede22df6e0c84462d52c69561efd40a050
pywako <wongaliceyy@gmail.com>
2017-10-11 14:27:18 +0200

    modification message contrainte halfday

--- src/AppBundle/Validator/Constraints/ConstraintHalfDayBooking.php
+++ src/AppBundle/Validator/Constraints/ConstraintHalfDayBooking.php
@@ -12,7 +12,8 @@ use Symfony\Component\Validator\Constraint;
  */
 class ConstraintHalfDayBooking extends Constraint
 {
-    public $message = "{{ string }} : la réservation pour l'après midi n'est plus disponible pour aujourd'hui";
+    public $message = "{{ string }} : la réservation pour la journée n'est plus disponible à partir de 14h, 
+    veuillez sélectionner le type demi-journée";
 
     public function getTargets()
     {
